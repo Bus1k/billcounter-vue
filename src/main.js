e14a9b62373c62dd8effea7358a85274f1de8f18
grzegorz.bu <grzegorz.bu@modulesgarden.com>
2020-12-20 14:50:07 +0100

    move router to separate file

--- src/main.js
+++ src/main.js
@@ -1,33 +1,13 @@
 import { createApp } from 'vue';
-import { createRouter, createWebHistory } from 'vue-router';
+import App from './App.vue';
+import Router from './router/router.js';
 
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import App from './App.vue';
-import Login from './views/Login.vue';
-import Register from './views/Register.vue';
-import Dashboard from './views/Dashboard.vue';
-
-const router = createRouter({
-    history: createWebHistory(),
-    routes: [{
-            path: '/login',
-            component: Login
-        },
-        {
-            path: '/register',
-            component: Register
-        },
-        {
-            path: '/dashboard',
-            component: Dashboard
-        }
-    ]
-});
 
 const app = createApp(App);
 
-app.use(router);
+app.use(Router);
 
 app.mount('#app');
---
+++ src/router/router.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Login from '../views/Login.vue';
+import Register from '../views/Register.vue';
+import Dashboard from '../views/Dashboard.vue';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [{
+            path: '/login',
+            component: Login
+        },
+        {
+            path: '/register',
+            component: Register
+        },
+        {
+            path: '/dashboard',
+            component: Dashboard
+        }
+    ]
+});
+
+export default router;
